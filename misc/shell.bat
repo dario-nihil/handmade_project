f0a2c5ee208d2cfb79260cf0f25c1019870a17dc
dario-nihil <dario.nihil@gmail.com>
2023-11-01 21:56:32 +0100

    initial commit

---
+++ misc/shell.bat
@@ -0,0 +1,3 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+set path=D:\Projects\c_projects\handmade_project\misc;%path%
